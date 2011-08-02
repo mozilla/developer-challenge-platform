360168d552c27038dfa6a1232c4f4b04ae0fbe36
Richard Taylor <moomerman@gmail.com>
2011-08-02 09:07:50 +0100

    attempt editing

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,6 +1,7 @@
 class AttemptsController < ApplicationController
   before_filter :authorize_user
   before_filter :challenge_requried
+  before_filter :attempt_required, :except => [:new, :create]
   
   def new
     @attempt = Attempt.new
@@ -15,8 +16,20 @@ class AttemptsController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    @attempt.update_attributes(params[:attempt])
+    redirect_to @attempt.user
+  end
+  
   private
     def challenge_requried
       @challenge = Challenge.find_by_id!(params[:challenge_id])
+    end
+    
+    def attempt_required
+      @attempt = Attempt.find_by_id!(params[:id])
     end
 end
--- db/migrate/20110730114910_create_attempts.rb
+++ db/migrate/20110730114910_create_attempts.rb
@@ -3,6 +3,7 @@ class CreateAttempts < ActiveRecord::Migration
     create_table :attempts do |t|
       t.references :challenge, :null => false
       t.references :user, :null => false
+      t.references :language, :null => false
       t.string :repository_url
       t.text   :description, :null => false
       t.timestamps
