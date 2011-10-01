4dd8d0460772c25976ae60a1678a6e439f34ede3
Richard Taylor <moomerman@gmail.com>
2011-10-01 15:06:03 +0100

    looking for config via env for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,13 @@
 github_config = YAML.load_file(File.join(Rails.root, "config/github.yml"))[Rails.env] rescue nil || {}
 twitter_config = YAML.load_file(File.join(Rails.root, "config/twitter.yml"))[Rails.env] rescue nil || {}
 
+github_client_id = ENV['github_client_id'] || github_config['client_id']
+github_secret = ENV['github_secret'] || github_config['secret']
+
+twitter_consumer_key = ENV['twitter_consumer_key'] || twitter_config['consumer_key']
+twitter_consumer_secret = ENV['twitter_consumer_secret'] || twitter_config['consumer_secret']
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, github_config['client_id'], github_config['secret']
-  provider :twitter, twitter_config['consumer_key'], twitter_config['consumer_secret']
+  provider :github, github_client_id, github_secret
+  provider :twitter, twitter_consumer_key, twitter_consumer_secret
 end
